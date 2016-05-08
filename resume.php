32535e6a25a50c187f229d10729607e2aa7760f9
Arjun Sreekumar <ensei-tankado@users.noreply.github.com>
2016-05-08 10:51:51 +0530

    Update resume.php
    for loop implemented

--- resume.php
+++ resume.php
@@ -1,52 +1,36 @@
 <?php
-if(!empty($_POST['cv']))
-{
-  $name = $_POST['firstname'] . ' ' . $_POST['lastname'];
-  $gender = $_POST['gender'];
-  $email = $_POST['email'];
-  $exp = $_POST['exp'];
-  $dob = $_POST['dob'];
-  $number = $_POST['number'];
-require("fpdf/fpdf.php");
-$pdf = new FPDF();
-$pdf->AddPage();
-$pdf->SetFont("Arial","B",16);
-$pdf->Cell(0,10,"RESUME",0,1,"C");
-$pdf->Cell(0,10,"",0,1);
-$pdf->Cell(40,10,"Name",0,0);
-$pdf->Cell(5,10,":",0,0);
-$pdf->Cell(50,10,"{$name}",0,1);
+     if(!empty($_POST['cv']))
+          {
+          $name = $_POST['firstname'] . ' ' . $_POST['lastname'];
+          $gender = $_POST['gender'];
+          $email = $_POST['email'];
+          $exp = $_POST['exp'];
+          $dob = $_POST['dob'];
+          $number = $_POST['number'];
+          $array=array($name,$dob,$gender,$number,$email,$exp);
+          $titles=array("Name","Date of Birth","Gender","Ph no","Email","Experience");
 
-$pdf->Cell(0,10,"",0,1);
-$pdf->Cell(40,10,"Date of Birth",0,0);
-$pdf->Cell(5,10,":",0,0);
-$pdf->Cell(50,10,"{$dob}",0,1);
+          require("fpdf/fpdf.php");
 
-$pdf->Cell(0,10,"",0,1);
-$pdf->Cell(40,10,"Gender",0,0);
-$pdf->Cell(5,10,":",0,0);
-$pdf->Cell(50,10,"{$gender}",0,1);
+          $pdf = new FPDF();
+          $pdf->AddPage();
+          $pdf->SetFont("Arial","B",16);
 
-$pdf->Cell(0,10,"",0,1);
-$pdf->Cell(40,10,"Ph no",0,0);
-$pdf->Cell(5,10,":",0,0);
-$pdf->Cell(50,10,"{$number}",0,1);
+          $pdf->Cell(0,10,"RESUME",0,1,"C");
 
-$pdf->Cell(0,10,"",0,1);
-$pdf->Cell(40,10,"Email",0,0);
-$pdf->Cell(5,10,":",0,0);
-$pdf->Cell(50,10,"{$email}",0,1);
+          for($i=0;$i<6;$i++)
+                   {
+                       $pdf->Cell(0,10,"",0,1);
+                       $pdf->Cell(40,10,"{$titles[$i]}",0,0);
+                       $pdf->Cell(5,10,":",0,0);
+                       $pdf->Cell(50,10,"{$array[$i]}",0,1);
+                   }
 
-$pdf->Cell(0,10,"",0,1);
-$pdf->Cell(40,10,"Experience",0,0);
-$pdf->Cell(5,10,":",0,0);
-$pdf->Cell(50,10,"{$exp}",0,1);
+          $pdf->SetFont("Arial","I",16);
 
-$pdf->SetFont("Arial","I",16);
-
-$pdf->Cell(0,10,"",0,1);
-$pdf->Cell(0,10,"",0,1);
-$pdf->Cell(0,10,"Signature",0,0,"R");
-$pdf->output();
-}
+          $pdf->Cell(0,10,"",0,1);
+          $pdf->Cell(0,10,"",0,1);
+          $pdf->Cell(0,10,"Signature",0,0,"R");
+          $pdf->output();
+          }
 ?>
