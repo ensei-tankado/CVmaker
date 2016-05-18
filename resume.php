9efcfd828c77191556969037550530653e8e076b
Arjun Sreekumar <ensei-tankado@users.noreply.github.com>
2016-05-18 11:22:38 +0530

    Update resume.php
    two previously used arrays were replaced by a single associative array.

--- resume.php
+++ resume.php
@@ -1,37 +1,36 @@
 <?php
      if(!empty($_POST['cv']))
-          {
-          $name = $_POST['firstname'] . ' ' . $_POST['lastname'];
-          $gender = $_POST['gender'];
-          $email = $_POST['email'];
-	  $education = $_POST['education'];
-          $exp = $_POST['exp'];
-          $dob = $_POST['dob'];
-          $number = $_POST['number'];
-          $array=array($name,$dob,$gender,$number,$email,$education,$exp);
-          $titles=array("Name","Date of Birth","Gender","Ph no","Email","Education","Experience");
+        {
+        $name = $_POST['firstname'] . ' ' . $_POST['lastname'];
+        $gender = $_POST['gender'];
+        $email = $_POST['email'];
+	$education = $_POST['education'];
+        $exp = $_POST['exp'];
+        $dob = $_POST['dob'];
+        $number = $_POST['number'];
+        $arr=array('Name'=>$name,'Date of Birth'=>$dob,'Gender'=>$gender,'Ph no'=>$number,'Email'=>$email,'Education'=>$education,'Experience'=>$exp);
 
-          require("fpdf/fpdf.php");
+   	require("fpdf/fpdf.php");
 
-          $pdf = new FPDF();
-          $pdf->AddPage();
-          $pdf->SetFont("Arial","B",16);
+        $pdf = new FPDF();
+        $pdf->AddPage();
+        $pdf->SetFont("Arial","B",16);
 
-          $pdf->Cell(0,10,"RESUME",0,1,"C");
+        $pdf->Cell(0,10,"RESUME",0,1,"C");
 
-          for($i=0;$i<count($array);$i++)
-                   {
-                       $pdf->Cell(0,10,"",0,1);
-                       $pdf->Cell(40,10,"{$titles[$i]}",0,0);
-                       $pdf->Cell(5,10,":",0,0);
-                       $pdf->Cell(50,10,"{$array[$i]}",0,1);
-                   }
+        foreach ($arr as $key => $value)
+                {
+                	$pdf->Cell(0,10,"",0,1);
+                        $pdf->Cell(40,10,"{$key}",0,0);
+                        $pdf->Cell(5,10,":",0,0);
+                        $pdf->Cell(50,10,"{$value}",0,1);
+                }
 
-          $pdf->SetFont("Arial","I",16);
+        $pdf->SetFont("Arial","I",16);
 
-          $pdf->Cell(0,10,"",0,1);
-          $pdf->Cell(0,10,"",0,1);
-          $pdf->Cell(0,10,"Signature",0,0,"R");
-          $pdf->output();
-          }
+        $pdf->Cell(0,10,"",0,1);
+        $pdf->Cell(0,10,"",0,1);
+        $pdf->Cell(0,10,"Signature",0,0,"R");
+        $pdf->output();
+        }
 ?>
